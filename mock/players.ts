1d14d499de3ae68dd891e7f9d325181ea526c5a6
DevilHe <929673054@qq.com>
2020-12-05 16:21:03 +0800

    feat:模拟玩家数据和英雄相关接口

--- mock/players.ts
+++ mock/players.ts
@@ -1,6 +1,38 @@
 import { Response, Request } from 'express'
+import { Hero, Player } from '../src/api/types'
+import { heros } from './heros'
+import faker from 'faker'
+
+faker.locale = 'zh_CN'
+
+// 模拟玩家数据
+const playerCount = 100
+const playerList: Player[] = []
+for (let i = 0; i < playerCount; i++) {
+  playerList.push({
+    id: i,
+    accountname: faker.name.findName(),
+    nickname: faker.name.findName(),
+    avatar: faker.image.avatar(),
+    bravepoints: faker.random.number(1000),
+    exp: faker.random.number(100000),
+    level: faker.random.number(30),
+    rank: faker.random.number(200),
+    wanttoplay: getWanttoplay(),
+    winningstreak: faker.random.number(10)
+  })
+}
+
+// 模拟想用英雄：三个且不能重复
+function getWanttoplay() {
+  const wanttoplay: Set<Hero> = new Set()
+  while (wanttoplay.size < 3) {
+    wanttoplay.add(heros[faker.random.number(9)])
+  }
+  return Array.from(wanttoplay)
+}
 
 // 路由实现
 export const getPlayers = (req: Request, res: Response) => {
-  res.json([1, 2, 3])
+  res.json(playerList)
 }
