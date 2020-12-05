30b5c807ed94a2d2f5cd5280b26d37ec992eaeac
DevilHe <929673054@qq.com>
2020-12-05 16:34:20 +0800

    feat:mock生成的数据过滤、分页

--- mock/players.ts
+++ mock/players.ts
@@ -34,5 +34,24 @@ function getWanttoplay() {
 
 // 路由实现
 export const getPlayers = (req: Request, res: Response) => {
-  res.json(playerList)
+  // 从查询参数中获取分页、过滤关键词等参数
+  const { accountname, page = 1, limit = 10 } = req.query
+
+  // 过滤
+  const mockList = playerList.filter(item => {
+    if (accountname && item.accountname.indexOf((accountname as string)) < 0) {
+      return false
+    }
+    return true
+  })
+
+  // 分页
+  const pageList = mockList.filter((item, index) => index < (limit as number) * (page as number) && index >= (limit as number) * ((page as number) - 1))
+  res.json({
+    code: 20000,
+    data: {
+      total: mockList.length,
+      items: pageList
+    }
+  })
 }
