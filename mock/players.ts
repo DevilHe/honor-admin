e46d47527b27dd16cabf6bc32029612cfeecdc00
DevilHe <929673054@qq.com>
2020-12-05 12:44:07 +0800

    feat:定义一个简单接口players

--- mock/players.ts
+++ mock/players.ts
@@ -1,2 +1,6 @@
+import { Response, Request } from 'express'
+
 // 路由实现
-// export const getPlayers = (req, res) => {}
+export const getPlayers = (req: Request, res: Response) => {
+  res.json([1, 2, 3])
+}
