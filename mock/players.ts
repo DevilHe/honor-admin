c10b11fffda5d78a47a60047d66a5600ba24d46a
DevilHe <929673054@qq.com>
2020-12-06 13:41:13 +0800

    feat:实现player相关接口(创建更新删除)

--- mock/players.ts
+++ mock/players.ts
@@ -55,3 +55,65 @@ export const getPlayers = (req: Request, res: Response) => {
     }
   })
 }
+
+// 根据id获取玩家信息
+export const getPlayer = (req: Request, res: Response) => {
+  const { id } = req.params
+  for (const player of playerList) {
+    if (player.id.toString() === id) {
+      res.json({
+        code: 20000,
+        data: {
+          player
+        }
+      })
+    }
+  }
+
+  // 没找到响应数据，报错
+  res.json({
+    code: 70001,
+    message: '没有找到相应的玩家信息'
+  })
+}
+
+// 创建玩家
+export const createPlayer = (req: Request, res: Response) => {
+  // 新增的英雄数据
+  const { player } = req.body
+
+  // 直接返回
+  res.json({
+    code: 20000,
+    data: {
+      player
+    }
+  })
+}
+
+// 更新玩家
+export const updatePlayer = (req: Request, res: Response) => {
+  const { id } = req.params
+  const { player } = req.body
+  for (const item of playerList) {
+    if (item.id.toString() === id) {
+      return res.json({
+        code: 20000,
+        data: {
+          player
+        }
+      })
+    }
+  }
+  res.json({
+    code: 70001,
+    message: '没有找到相应的玩家信息'
+  })
+}
+
+// 删除玩家
+export const deletePlayer = (req: Request, res: Response) => {
+  res.json({
+    code: 20000
+  })
+}
