86b35ee53d144f21fec4447d70fda953dbd71789
DevilHe <929673054@qq.com>
2020-12-10 21:39:17 +0800

    feat:玩家列表添加分页

--- src/views/players/list.vue
+++ src/views/players/list.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app-container">
+    <!-- 列表 -->
     <el-table
       v-loading="listLoading"
       :data="list"
@@ -15,6 +16,15 @@
         <template v-slot="{row}">{{row.accountname}}</template>
       </el-table-column>
     </el-table>
+
+    <!-- 分页 -->
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="listQuery.page"
+      :limit.sync="listQuery.limit"
+      @pagination="getList"
+    ></pagination>
   </div>
 </template>
 
@@ -22,13 +32,29 @@
 import { Component, Vue } from 'vue-property-decorator'
 import { getPlayers } from '@/api/players'
 import { Player } from '@/api/types'
-@Component
+import Pagination from '@/components/Pagination/index.vue'
+
+@Component({
+  name: 'PlayerList',
+  components: {
+    Pagination
+  }
+})
 export default class list extends Vue {
   // 玩家列表数据
   list: Player[] = []
 
   // 加载状态
   listLoading = true
+
+  // 总条目数量
+  total = 0
+
+  // 查询
+  listQuery = {
+    page: 1, // 默认第一页
+    limit: 10 // 每页条数
+  }
 
   created() {
     // 获取列表数据
@@ -38,9 +64,12 @@ export default class list extends Vue {
   // 获取列表
   async getList() {
     this.listLoading = true
-    const { data } = await getPlayers({})
-    console.log(data)
+    const { data } = await getPlayers(this.listQuery)
+    // console.log(data)
+    // 设置列表数据
     this.list = data.players
+    // 设置数据总条数
+    this.total = data.total
     this.listLoading = false
   }
 }
